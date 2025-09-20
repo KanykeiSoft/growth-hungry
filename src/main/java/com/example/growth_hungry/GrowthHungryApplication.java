d85be11a90b901dd5c0cb90f2105a96e621b11ca
Kanykei <kana123k@gmail.com>
2025-09-20 08:49:00 -0400

    Initial commit

---
+++ src/main/java/com/example/growth_hungry/GrowthHungryApplication.java
@@ -0,0 +1,13 @@
+package com.example.growth_hungry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthHungryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthHungryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/growth_hungry/GrowthHungryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.growth_hungry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthHungryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
