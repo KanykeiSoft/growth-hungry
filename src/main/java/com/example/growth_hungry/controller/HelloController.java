fd57075e8d3d3842c8b6e67c315ff327319789d1
Kanykei <kana123k@gmail.com>
2025-09-27 15:08:35 -0400

    readme

--- src/main/java/com/example/growth_hungry/controller/HelloController.java
+++ src/main/java/com/example/growth_hungry/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.growth_hungry;
+package com.example.growth_hungry.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
