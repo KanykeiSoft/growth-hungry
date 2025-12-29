26309fe1970bb35b919807c4e778c190f892aa31
Kanykei <kana123k@gmail.com>
2025-12-28 22:44:25 -0500

    Add unit tests for ChatServiceImpl with Mockito and SecurityContext

--- src/main/java/com/example/growth_hungry/model/chat/ChatMessage.java
+++ src/main/java/com/example/growth_hungry/model/chat/ChatMessage.java
@@ -52,6 +52,10 @@ public class ChatMessage {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getContent() {
         return content;
     }
